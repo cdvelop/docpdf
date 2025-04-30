3b997016407067634b970d46dddf247f374d12e2
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2025-04-29 22:58:14 -0400

    fix alignment en chart

--- docChart.go
+++ docChart.go
@@ -431,6 +431,8 @@ func (c *docChart) Draw() error {
 
 	// Usar los bytes del buffer directamente sin necesidad de archivo temporal
 	docImage := c.doc.AddImage(buf.Bytes())
+	// ajustar la alineación según la configuración
+	docImage.alignment = c.alignment
 
 	err = docImage.Draw()
 	if err != nil {
